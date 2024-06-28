14443ff52e8a8bd575ca54b5ca92704400479f26
Jihoo Ahn <lucidorangee@gmail.com>
2024-06-28 16:12:23 -0400

    Removed @import URL

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -2,8 +2,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap');
-
   body {
     margin: 0;
     padding: 0;
